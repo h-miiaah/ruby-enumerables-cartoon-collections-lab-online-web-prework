0a5c265522d9551d08df2a1597a8d10379bf6d46
Haim D <haim11293@aol.com>
2019-06-19 00:40:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,9 @@ def summon_captain_planet(planeteer_calls)# code an argument here
   planeteer_calls.map{|call| "#{call.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(long_calls)# code an argument here
   # Your code here
+  long_calls.any? {|call| call.length >= 4}
 end
 
 def find_the_cheese# code an argument here
